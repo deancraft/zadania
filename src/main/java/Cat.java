a2723242e8d5178de6770c118ea5c6525c961479
Łukasz Chrzanowski <deancraft@gmail.com>
2019-01-13 21:01:05 +0100

    Initial commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,6 @@
+public class Cat extends Animal {
+
+    protected void eat() {
+        System.out.printf("Jem kitycat");
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.eat();
+
+        List<String> lista = new ArrayList<String>();
+        lista = new LinkedList<String>();
+    }
+}
